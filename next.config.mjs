e64b0969bb1d9cd47465b2d4b6648b4c9e1a8c49
Shreyan <shreyan010@gmail.com>
2024-10-13 22:19:13 +0530

    push02

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    // Disable ESLint during builds
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    // Disable TypeScript type checking during builds
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
 
 export default nextConfig;
